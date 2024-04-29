7d56d938ec314524f7a1049d4924cf48ff8e7d55
martingom4 <martingomez9636@gmail.com>
2024-04-29 11:46:47 -0500

    Add project files.

---
+++ C#2/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Console.WriteLine("Manda tu nombre");
+
+//int numero_1= int.Parse(Console.ReadLine());
+
+//int Numero_1 = int.Parse(Console.ReadLine());
+
+
+//Console.WriteLine(numero_1);
+
+//Console.WriteLine(Numero_1);
+
+//int max = int.MaxValue;
+//    Console.WriteLine(max);
+//DateTime nombre= new DateTime( 2332, 33,23 );
+
+//Console.WriteLine(nombre);
+
+using C_2;
+
+int addnumbers(int a, int b, int c )
+{
+    c = 100;
+    int sum = a + b + c;
+    return sum;
+}
+
+
+//Console.WriteLine(addnumbers(1, 2,2 ));
+
+
+//Console.WriteLine(Utilities.Hola(1,2));
+
+//string a = Console.ReadLine();
+
+string texto = "alha";
+texto = texto.ToUpper();
+char[] array = texto.ToCharArray();
+Array.Reverse(array);
+string textoInvertido = new(array);
+Console.WriteLine(textoInvertido);
+
+
+textoInvertido= textoInvertido.ToUpper();
+Console.WriteLine(textoInvertido);
+bool x = textoInvertido.Contains(texto);
+Console.WriteLine(x);
---
+++ C#2/Utilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_2
+{
+    internal class Utilities
+    {
+        public static void Argumentados() { 
+            
+            int c= 0;
+            int a = 0;
+        
+        }
+
+        public static int Hola(int a,int b) => a + b;
+
+
+
+    }
+}
